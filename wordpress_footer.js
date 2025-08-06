53bec652a36d2fd43c48d8dae7aa64bed8acd8a3
LadislavKubini <129084192+LadislavKubini@users.noreply.github.com>
2025-08-06 13:14:28 +0200

    Add files via upload

--- wordpress_footer.js
+++ wordpress_footer.js
@@ -1,11 +1,11 @@
       // Načítanie kratkeho zoznamu pracovných ponúk pre stlpec "Kariera" vo Footer.
       async function Footer_fill_ponuky() {
+        const ul_container = document.getElementById("id-ul-ponuky");
         fetch("https://www.enitech.sk/wp-json/wp/v2/posts?per_page=5")
           .then(res => res.json())
           .then(data => {
             const sorted = data.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-            const ul_container = document.getElementById("id-ul-ponuky");
             ul_container.innerHTML = ""; // Vyčisti "Načítavam..."
             sorted.forEach(post => {
               const li_element = document.createElement("li");
